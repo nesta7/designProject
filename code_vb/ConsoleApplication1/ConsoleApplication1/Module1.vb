fc88e220018e3ea285b4d16a94c7cb9cc993fc00
maxbombix <max.mentha@epfl.ch>
2015-04-04 18:57:49 +0200

    code_vbnet_debut

---
+++ code_vb/ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Windows.Forms
+Module Module1
+    Sub Main()
+        Dim path = "C:\Users\max\Documents\cours MA2\design project\dossier_partage\code_vb\polygones.csv"
+        Dim lines = IO.File.ReadAllLines(path)
+        Dim tbl = New DataTable
+        Dim colCount = lines.First.Split(","c).Length
+        For i As Int32 = 1 To colCount
+            tbl.Columns.Add(New DataColumn("Column_" & i, GetType(Int32)))
+        Next
+        'tbl.Columns.Add(New DataColumn("Polygone_id", GetType(Int32)))
+        'tbl.Columns.Add(New DataColumn("Altitude", GetType(Double)))
+        For Each line In lines
+            Dim objFields = From field In line.Split(","c)
+                         Select CType(Int32.Parse(field), Object)
+            Dim newRow = tbl.Rows.Add()
+            newRow.ItemArray = objFields.ToArray()
+        Next
+        MessageBox.Show(tbl.Rows(0)(1))
+
+    End Sub
+
+End Module
