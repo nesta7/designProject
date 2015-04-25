1dbf55598deb494a0b8f25990bb4f53a15673bfc
maxbombix <max.mentha@epfl.ch>
2015-04-25 15:46:23 +0200

    avancement_code

--- code_vb/ConsoleApplication1/ConsoleApplication1/Module1.vb
+++ code_vb/ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -149,6 +149,7 @@ Module Module1
         Dim inonde_past(lines_past.Length - 2, lines_polygon.Length - 1) As Integer 'MAX: plus tard on aura pas besoin de garder cette information. Juste les quelques dernières lignes. Celles d'avant auront déjà été prises en compte.
         'MAX: pour la prochaine variable: 1ere dimension = temps. 2eme dimension = polygone, 3eme dimension = stade en cours et pourcentage d'avancement du stade
         Dim state_time(lines_past.Length - 2, lines_polygon.Length - 1, 2) As Integer
+        'Dim state_output(2) As integer
 
         For t = 0 To lines_past.Length - 2 'MAX: on fait -2 car la première ligne ne doit pas etre considérée puisque c'est les titres de colonnes et parce que l'on part de 0
             For i = 0 To lines_polygon.Length - 1
@@ -209,20 +210,12 @@ Module Module1
         Next
 
 
+        Dim youhou(4, 4) As Double
 
-        Console.WriteLine()
+        Console.WriteLine(youhou.Length)
         Console.Read()
     End Sub
 
-
-    'Function updating the lavaire state
-    'By Morgan Bruhin
-    'PS didn't use "degre-jour" didn't understand well enought it
-    'Using percentage of grow per state
-
-    'Advantage : would allow to have more than one state per polygone (more than only one larval type)
-
-    'Function that compute the Increase percentage for state cs at temperature T for 1 day
     Function LarvaModel(ByVal cs As Integer, ByVal T As Double) As Double
 
         Dim Prop As Double = 0.0
@@ -301,6 +294,96 @@ Module Module1
 
         Return PercLarvaState
     End Function
+
+    ''VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN
+    ''Function updating the lavaire state
+    ''By Morgan Bruhin
+    ''PS didn't use "degre-jour" didn't understand well enought it
+    ''Using percentage of grow per state
+
+    ''Advantage : would allow to have more than one state per polygone (more than only one larval type)
+
+    ''Function that compute the Increase percentage for state cs at temperature T for 1 day
+    'Function LarvaModel(ByVal cs As Integer, ByVal T As Double) As Double
+
+    '    Dim Prop As Double = 0.0
+    '    Dim i As Integer
+    '    Dim j As Integer
+    '    Dim noCol As Integer = 4
+    '    Dim Model(4, noCol) As Double
+
+    '    'Reference at the bottom
+    '    Dim temp() As Double = {15, 20, 25, 30, 35}
+    '    Dim L1() As Double = {5.6, 3.0, 2.1, 1.4, 1.7}
+    '    Dim L2() As Double = {3.3, 1.4, 1.2, 1.3, 1.2}
+    '    Dim L3() As Double = {4.6, 2.1, 1.2, 1.4, 2.4}
+    '    Dim L4() As Double = {13.4, 4.1, 3.3, 3.0, 6.8}
+
+    '    'Could probably be inproved
+    '    For i = 0 To noCol
+    '        Model(0, i) = temp(i)
+    '        Model(1, i) = L1(i)
+    '        Model(2, i) = L2(i)
+    '        Model(3, i) = L3(i)
+    '        Model(4, i) = L4(i)
+    '    Next
+    '    If T < Model(0, 0) Or T > Model(0, noCol) Then
+    '        Console.WriteLine("Temperature " & T & "°C out of range [ " & Model(0, 0) & ":" & Model(0, noCol) & " ]")
+    '    Else
+    '        For i = 0 To noCol - 1
+    '            If T >= temp(i) And T < temp(i + 1) Then
+    '                'Compute a linear estimation of the require number of days to grow up
+    '                'then compute the actual proportion of growing since the simulation is at a daily scale
+    '                Prop = 1 / (Model(cs, i) + (T - Model(0, i)) / (Model(0, i + 1) - Model(0, i)) * (Model(cs, i + 1) - Model(cs, i)))
+    '            End If
+    '        Next
+    '    End If
+    '    'Display test
+    '    'For i = 0 To 4
+    '    '   For j= 0 To noCol
+    '    '      Console.WriteLine("Model "& Model(i,j))
+    '    ' Next
+    '    'Next
+
+    '    Return Prop
+    'End Function
+
+    ''Should be a method but I'mnot good at it
+    ''Function that modify the Percentage in the LarvaStates and return the new one after applying the LarvalModel to each require state
+    ''By Morgan Bruhin
+    'Function ChangeState(ByVal PercLarvaState() As Double, ByVal T As Double) As Double()
+    '    Dim i As Integer
+    '    Dim index As Integer = -1
+    '    For i = 0 To 3
+    '        'if the larva aren't present at the current state i then the percentage is -1
+    '        If PercLarvaState(i) <> -1 Then
+    '            'Increase the Larva growth percentage
+    '            PercLarvaState(i) = PercLarvaState(i) + LarvaModel(i, T)
+    '            'if the percentage is up to 1 the state is complete
+    '            If PercLarvaState(i) >= 1.0 Then
+    '                'if the state isn't the last one activate the next
+    '                If i < 3 Then
+    '                    If PercLarvaState(i + 1) = -1 Then
+    '                        index = (i + 1)
+    '                    Else
+    '                        'TODO : Decide what to do if a younger population arrive befor the previous fully grows
+    '                    End If
+    '                Else
+    '                    'TODO : decide what to do exactly for T4 fully grow
+    '                End If
+    '                'At the end : desactivate the current state
+    '                PercLarvaState(i) = -1
+    '            End If
+    '            'Activate the next state without computing a new day
+    '        ElseIf i = index Then
+    '            PercLarvaState(i) = 0 ' TODO : Compute the difference with the previous state and compute a proportional increase
+    '        End If
+    '    Next
+
+    '    Return PercLarvaState
+    'End Function
+    ''VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN-VERSION-DE-BASE-DE-MORGAN
+
 
 End Module
 
